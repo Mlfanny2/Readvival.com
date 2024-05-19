58ce58d818b039e0d0c4a52dda4f0cf18142df77
Readvival.com <167634861+Mlfanny2@users.noreply.github.com>
2024-05-19 22:50:43 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      READVIVAL
+      <nav> 
+      <ul className="nav">
+      <li>Home</li>
+       <li>Quiz</li>
+       <li>Videos</li>
+        <li>Dictionary</li>
+      </ul>
+      </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
